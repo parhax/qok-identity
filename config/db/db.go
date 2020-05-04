05851005d22a23978e2cfbe66f2c198087661047
Parix <parham.toupaebrahimi@gmail.com>
2020-05-05 04:09:22 +0430

    everything works fine with dockerization

--- config/db/db.go
+++ config/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -10,7 +11,9 @@ import (
 
 func GetDBCollection() (*mongo.Collection, error) {
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	url := os.Getenv("MONGO_URL")
+	// client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(url))
 
 	// Check the connection
 	err = client.Ping(context.TODO(), nil)
